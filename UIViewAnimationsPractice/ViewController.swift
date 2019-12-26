868ce660dda5099b7afc8fc87c072088a5d9525a
Jules Lee <jackconsolidated@gmail.com>
2019-12-27 00:21:51 +0800

    Initially make the box go up right down left then back to the center Every move happens every one second

--- UIViewAnimationsPractice/ViewController.swift
+++ UIViewAnimationsPractice/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(blockTapped))
         block.addGestureRecognizer(tap)
         block.isUserInteractionEnabled = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.up()
+        }
     }
     
     @objc func blockTapped() {
@@ -30,7 +34,44 @@ class ViewController: UIViewController {
             self.block.frame = CGRect(x: newX, y: newY, width: newSize, height: newSize)
         }
     }
-
+    
+    func up() {
+        UIView.animate(withDuration: 1, animations: {
+            self.block.frame.origin = CGPoint(x: self.view.center.x - (self.block.frame.width / 2), y: 0)
+        }) { (success) in
+            self.right()
+        }
+    }
+    
+    func right() {
+        UIView.animate(withDuration: 1, animations: {
+            self.block.frame.origin = CGPoint(x: self.view.frame.width - self.block.frame.width, y: self.view.center.y - (self.block.frame.size.height / 2))
+        }) { (success) in
+            self.down()
+        }
+    }
+    
+    func left() {
+        UIView.animate(withDuration: 1, animations: {
+            self.block.frame.origin = CGPoint(x: 0, y: self.view.center.y - (self.block.frame.size.height / 2))
+        }) { (success) in
+            self.reset()
+        }
+    }
+    
+    func down() {
+        UIView.animate(withDuration: 1, animations: {
+            self.block.frame.origin = CGPoint(x: self.view.center.x - (self.block.frame.width / 2), y: self.view.frame.height - self.block.frame.height)
+        }) { (success) in
+            self.left()
+        }
+    }
+    
+    func reset() {
+        UIView.animate(withDuration: 1) {
+            self.block.center = CGPoint(x: self.view.center.x, y: self.view.center.y)
+        }
+    }
 
 }
 
