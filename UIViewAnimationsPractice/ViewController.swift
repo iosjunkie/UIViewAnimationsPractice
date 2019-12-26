8871b472630294474fa67011c40f0aef80650457
Jules Lee <jackconsolidated@gmail.com>
2019-12-27 00:01:57 +0800

    A 60x60 box that expands 1.5 times when tapped.

--- UIViewAnimationsPractice/ViewController.swift
+++ UIViewAnimationsPractice/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(blockTapped))
+        block.addGestureRecognizer(tap)
+        block.isUserInteractionEnabled = true
+    }
+    
+    @objc func blockTapped() {
+        UIView.animate(withDuration: 1.2) {
+            let sizeMultiplier = CGFloat(1.5) // 1.5 times bigger than it was bigger
+            let currentBlockFrame = self.block.frame
+            let newSize = currentBlockFrame.width * sizeMultiplier
+            let newX = currentBlockFrame.origin.x - (newSize - currentBlockFrame.width) / 2
+            let newY = currentBlockFrame.origin.y - (newSize - currentBlockFrame.height) / 2
+            self.block.frame = CGRect(x: newX, y: newY, width: newSize, height: newSize)
+        }
     }
 
 
