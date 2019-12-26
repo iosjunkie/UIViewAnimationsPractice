abab95b4f83750f3e14e33533b9d1b7e06448240
Jules Lee <jackconsolidated@gmail.com>
2019-12-26 23:42:31 +0800

    Initial Commit

---
+++ UIViewAnimationsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewAnimationsPractice
+//
+//  Created by Jules Lee on 12/26/19.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
